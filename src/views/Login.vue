c76887581cd8a6a49ac70cf234b22788679b1476
emillevic <emillesampaio2013@gmail.com>
2021-08-29 16:01:36 -0300

    Adding vuetify

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row class="justify-center align-center">
+      <v-col class="text-center">
+        <h1>Login</h1>
+        <v-btn color="primary">Fazer login com a Huggy</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    name: 'Login',
+  }
+</script>
